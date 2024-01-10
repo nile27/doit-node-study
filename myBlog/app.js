1ad8ec2fb6902d51ffc218452f12a6f7852158af
nile27 <ddj03104@gmail.com>
2024-01-10 17:03:43 +0900

    화면 레이아웃

--- myBlog/app.js
+++ myBlog/app.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const expressLayout = require("express-ejs-layouts");
+const connectDb = require("./config/db");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+connectDb();
+
 app.use(expressLayout);
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.set("views", "./views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
--- myBlog/routes/main.js
+++ myBlog/routes/main.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const mainLayout = "../views/layouts/main.ejs";
+const Post = require("../models/post");
+const asyncHandler = require("express-async-handler");
 
-router.get(["/", "/home"], (req, res) => {
-  const locals = {
-    title: "HOME",
-  };
+router.get(
+  ["/", "/home"],
+  asyncHandler(async (req, res) => {
+    const locals = {
+      title: "HOME",
+    };
 
-  res.render("index", { locals, layout: mainLayout });
-});
+    const data = await Post.find();
+    res.render("index", { locals, data, layout: mainLayout });
+  })
+);
+
+// 게시물 상세 보기
+// get /POST/:id
+
+router.get(
+  "/post/:id",
+  asyncHandler(async (req, res) => {
+    const data = await Post.findOne({ _id: req.params.id });
+    res.render("post", { data, layout: mainLayout });
+  })
+);
 
 router.get("/about", (req, res) => {
   const locals = {
@@ -19,3 +36,22 @@ router.get("/about", (req, res) => {
 });
 
 module.exports = router;
+
+// Post.insertMany([
+//   {
+//     title: "제목 1",
+//     body: "내용 1",
+//   },
+//   {
+//     title: "제목 2",
+//     body: "내용 2",
+//   },
+//   {
+//     title: "제목 3",
+//     body: "내용 3",
+//   },
+//   {
+//     title: "제목 4",
+//     body: "내용 4",
+//   },
+// ]);
